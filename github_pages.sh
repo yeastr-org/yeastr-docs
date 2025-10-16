c884ef9cadafa16ffb0729602420aa92ab3ab8be
castix <castix@autistici.org>
2025-10-16 23:34:19 +0200

    gh pages

---
+++ github_pages.sh
@@ -0,0 +1,35 @@
+#ACTIONS_ID_TOKEN_REQUEST_URL
+# should we?
+#
+
+set -e -x
+
+git branch -D gh-pages || true
+git checkout --orphan gh-pages
+
+for f in `find build/html -type f -regex '.*\.html$'` ; do
+    sed -i $f\
+        -r -e 's@(href="|src=")([\.\./]*)_static@\1\2static@g' \
+        -e 's@href="_modules@href="modules@g' \
+        -e 's@href="_sources@href="sources@g' \
+        -e 's@src="_images@src="images@g'
+done
+
+rm -rf build/html/{static,modules,sources,images}
+mv build/html/_static build/html/static
+mv build/html/_modules build/html/modules
+mv build/html/_sources build/html/sources
+mv build/html/_images build/html/images
+
+mv build/html docs
+
+touch docs/.nojekyll
+
+git add docs docs/.nojekyll
+git add -f docs/{static,modules,sources,images}
+git commit -m "pages"
+
+git push gh :gh-pages
+git push gh gh-pages
+git checkout dev
+# git clean -fdx -e venv
